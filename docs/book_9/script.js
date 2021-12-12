4e196cf1fce4254229078a2458a59d7710a6b84e
tthr <weather@shower.rash.jp>
2021-12-12 14:32:57 +0900

    mobile luxy delete

--- docs/book_9/script.js
+++ docs/book_9/script.js
@@ -77,7 +77,30 @@ $('.nav-link a[href*="#"]').click(function () {//全てのページ内リンク�
     btn.classList.toggle("tham-active");
 });
 
+
+
+var _ua = (function(u){
+    return {
+      Tablet:(u.indexOf("windows") != -1 && u.indexOf("touch") != -1 && u.indexOf("tablet pc") == -1) 
+        || u.indexOf("ipad") != -1
+        || (u.indexOf("android") != -1 && u.indexOf("mobile") == -1)
+        || (u.indexOf("firefox") != -1 && u.indexOf("tablet") != -1)
+        || u.indexOf("kindle") != -1
+        || u.indexOf("silk") != -1
+        || u.indexOf("playbook") != -1,
+      Mobile:(u.indexOf("windows") != -1 && u.indexOf("phone") != -1)
+        || u.indexOf("iphone") != -1
+        || u.indexOf("ipod") != -1
+        || (u.indexOf("android") != -1 && u.indexOf("mobile") != -1)
+        || (u.indexOf("firefox") != -1 && u.indexOf("mobile") != -1)
+        || u.indexOf("blackberry") != -1
+    }
+  })(window.navigator.userAgent.toLowerCase());
+
+  if(!_ua.Mobile&&!_ua.Tablet){
+
 luxy.init({
     wrapper: '#luxy',
     wrapperSpeed:  0.09
   });
+}
