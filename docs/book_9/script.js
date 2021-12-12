a5cf697c20409b7f9b059acec0f28a0228bf888e
tthr <weather@shower.rash.jp>
2021-12-12 14:38:18 +0900

    luxy

--- docs/book_9/script.js
+++ docs/book_9/script.js
@@ -79,28 +79,14 @@ $('.nav-link a[href*="#"]').click(function () {//全てのページ内リンク�
 
 
 
-var _ua = (function(u){
-    return {
-      Tablet:(u.indexOf("windows") != -1 && u.indexOf("touch") != -1 && u.indexOf("tablet pc") == -1) 
-        || u.indexOf("ipad") != -1
-        || (u.indexOf("android") != -1 && u.indexOf("mobile") == -1)
-        || (u.indexOf("firefox") != -1 && u.indexOf("tablet") != -1)
-        || u.indexOf("kindle") != -1
-        || u.indexOf("silk") != -1
-        || u.indexOf("playbook") != -1,
-      Mobile:(u.indexOf("windows") != -1 && u.indexOf("phone") != -1)
-        || u.indexOf("iphone") != -1
-        || u.indexOf("ipod") != -1
-        || (u.indexOf("android") != -1 && u.indexOf("mobile") != -1)
-        || (u.indexOf("firefox") != -1 && u.indexOf("mobile") != -1)
-        || u.indexOf("blackberry") != -1
-    }
-  })(window.navigator.userAgent.toLowerCase());
-
-  if(!_ua.Mobile&&!_ua.Tablet){
-
-luxy.init({
-    wrapper: '#luxy',
-    wrapperSpeed:  0.09
-  });
+if (navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('Android') > 0 && navigator.userAgent.indexOf('Mobile') > 0) {
+    // スマートフォン向けの記述
+} else if (navigator.userAgent.indexOf('iPad') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+    // タブレット向けの記述
+} else {
+    // PC向けの記述
+    luxy.init({
+        wrapper: '#luxy',
+        wrapperSpeed:  0.09
+      });
 }
