aeea07b20bcbeff52c28c0d01cf94434f07d80b7
tthr <weather@shower.rash.jp>
2021-12-14 22:12:06 +0900

    aspect

--- docs/book_9/script.js
+++ docs/book_9/script.js
@@ -1,13 +1,13 @@
 
 
-//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
-
 $('#page-link a[href*="#"]').click(function () {
 	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-	var pos = $(elmHash).offset().top-10;	//idの上部の距離を取得
+	var pos = $(elmHash).offset().top-0;	//idの上部の距離を取得
 	$('body,html').animate({scrollTop: pos}, 900); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
 	return false;
 });
+
+
 
 
 // ナビゲーション
@@ -89,3 +89,4 @@ $(window).on('scroll load', function () {
     $("#splash_logo").delay(1300).fadeOut('slow');
     $('body').css('display', 'block');
 });
+
